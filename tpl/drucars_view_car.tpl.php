4eb96c975901018315587e1774d41e19bc86aa70
Maxtorete <juanfr@gmail.com>
2012-06-30 20:13:24 +0200

    Fixed menu callbacks

--- tpl/drucars_view_car.tpl.php
+++ tpl/drucars_view_car.tpl.php
@@ -2,7 +2,7 @@
 <?php if ($car->isRented()):?>
 <h2><?php print t('This car is rented'); ?></h2>
 <ul>
-  <li><?php print t('Init date: @init_date', array('@init_date', date('d/m/Y', $car->getInitDate()))); ?></li>
-  <li><?php print t('End date: @end_date', array('@end_date', date('d/m/Y', $car->getEndDate()))); ?></li>
+  <li><?php print t('Init date: @init_date', array('@init_date' => date('d/m/Y', $car->getInitDate()))); ?></li>
+  <li><?php print t('End date: @end_date', array('@end_date' => date('d/m/Y', $car->getEndDate()))); ?></li>
 </ul>
 <?php endif;?>
